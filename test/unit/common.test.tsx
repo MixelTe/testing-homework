bc3fc5f8ba1b33e0b6d07e45c40d7b80cf8b44b1
Mike <63655075+MixelTe@users.noreply.github.com>
2022-11-16 23:17:07 +0300

    added two unit tests

--- test/unit/common.test.tsx
+++ test/unit/common.test.tsx
@@ -34,6 +34,39 @@ describe('Проверка общих требований', () => {
     it('Вёрстка должна адаптироваться под ширину экрана', () =>
     {
         const { container } = Render();
-        expect(container).toBeInstanceOf(HTMLElement);
+        expect("Знания").toBe("Достаточно для решения");
+    });
+    
+    it('В шапке отображаются ссылки на страницы магазина, а также ссылка на корзину', () =>
+    {
+        const { container } = Render();
+        const nav = container.querySelector("nav");
+        expect(nav).toBeDefined();
+        if (!nav) return;
+        const linkEls = nav.querySelectorAll("a");
+        const links: string[] = [];
+        linkEls.forEach(link => links.push(link.href));
+
+        expect(links.some(v => /\/store\/catalog$/.test(v))).toBeTruthy();
+        expect(links.some(v => /\/store\/delivery$/.test(v))).toBeTruthy();
+        expect(links.some(v => /\/store\/contacts$/.test(v))).toBeTruthy();
+        expect(links.some(v => /\/store\/cart$/.test(v))).toBeTruthy();
+    });
+
+    it('Название магазина в шапке должно быть ссылкой на главную страницу', () =>
+    {
+        const { container } = Render();
+        const brand = container.querySelector("nav .navbar-brand");
+        expect(brand).toBeTruthy();
+        expect(brand instanceof HTMLAnchorElement).toBeTruthy();
+        if (!brand || !(brand instanceof HTMLAnchorElement)) return;
+
+        expect(/\/store\/$/.test(brand.href)).toBeTruthy();
+    });
+
+    it('На ширине меньше 576px навигационное меню должно скрываться за "гамбургер"', () =>
+    {
+        const { container } = Render();
+        expect("Знания").toBe("Достаточно для решения");
     });
 });
